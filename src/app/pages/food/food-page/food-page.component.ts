57188022fc82154cd4bf48a72d06126cf2804ce4
RyoMatsuuchi <matsuuchi.ryo@suzukishouten.co.jp>
2019-09-14 01:25:28 +0900

    fix: foodのModalを実装

--- src/app/pages/food/food-page/food-page.component.ts
+++ src/app/pages/food/food-page/food-page.component.ts
@@ -1,17 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { JsonConvertService } from '../../../core/service/json-convert/json-convert.service';
 import { DomSanitizer, Title } from '@angular/platform-browser';
-import { BsModalService } from 'ngx-bootstrap';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap';
 import { Shop } from '../../../models/shop';
+import { BehaviorSubject } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-food-page',
   templateUrl: './food-page.component.html',
   styleUrls: ['./food-page.component.scss']
 })
-export class FoodPageComponent implements OnInit {
+export class FoodPageComponent implements OnInit, OnDestroy {
+  @ViewChild('shopModalTemplate', { static: true }) modalTemplate: TemplateRef<any>;
 
   shops: Shop[];
+  modalRef: BsModalRef;
+
+  selectedShop$: BehaviorSubject<Shop> = new BehaviorSubject<Shop>(null);
 
   constructor(private jsonConvertService: JsonConvertService,
               private titleService: Title,
@@ -21,9 +27,26 @@ export class FoodPageComponent implements OnInit {
 
   ngOnInit() {
     this.fetchShops();
+    this.selectedShop$.pipe(filter(v => !!v)).subscribe(() => {
+      this.openModal();
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (!this.selectedShop$.closed) {
+      this.selectedShop$.unsubscribe();
+    }
+  }
+
+  onShowFoodModal(shop: Shop) {
+    this.selectedShop$.next(shop);
   }
 
   private fetchShops() {
     this.jsonConvertService.fetchShops().subscribe(val => this.shops = val);
+  }
+
+  private openModal() {
+    this.modalRef = this.modalService.show(this.modalTemplate, { class: 'modal-lg' });
   }
 }
